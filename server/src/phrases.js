da212552aa063ae71f71de5f4aa4d6f7ad3cff8b
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-07-04 19:25:26 -0400

    Adicionando mensagens ao entrar e sair do chat e mudando a estrutura das mensagens

---
+++ server/src/phrases.js
@@ -0,0 +1,17 @@
+const phrases = {
+    joinChat: [
+        'acabou de conectar-se ao chat. Diga um olá.',
+        'acabou de cair de paraquedas no chat.',
+        'acabou de aterrisar.',
+        'entrou. Agora a conversa pode ficar mais interessante.',
+        'chegou. Salve-se quem puder!'
+    ],
+    leaveChat: [
+        'acabou de desconectar-se do chat sem dizer tchau.',
+        'disse tchau e desconectou-se do chat.',
+        'acabou de desconectar-se do chat.',
+        'acabou de sair do chat. Vocês podem voltar a conversar agora.'
+    ]
+}
+
+module.exports = phrases
--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,32 @@
 const io = require('socket.io')()
-const {JOIN_CHAT, RECEIVE_CLIENT_ID, LEAVE_CHAT, SEND_MESSAGE} = require('./constants')
+const {JOIN_CHAT, SEND_USER_CONFIRMATION, LEAVE_CHAT, SEND_MESSAGE} = require('./constants')
+const {joinChat, leaveChat} = require('./phrases')
 
 io.on('connection', (socket) => {
     socket.on(JOIN_CHAT, (username) => {
-        socket.emit(RECEIVE_CLIENT_ID, socket.id)
-        io.emit(JOIN_CHAT, username)
+        const user = {id: socket.id, name: username}
+        socket.emit(SEND_USER_CONFIRMATION, user)
+
+        const phrasePosition = generateRandomPosition(joinChat.length)
+        const joinChatMessage = `${username} ${joinChat[phrasePosition]}`
+        const message = {user: user, value: joinChatMessage}
+        io.emit(JOIN_CHAT, message)
     })
     socket.on(LEAVE_CHAT, (username) => {
-        io.emit(LEAVE_CHAT, username)
+        const user = {id: socket.id, name: username}
+        const phrasePosition = generateRandomPosition(leaveChat.length)
+        const leaveChatMessage = `${username} ${leaveChat[phrasePosition]}`
+        const message = {user: user, value: leaveChatMessage}
+        io.emit(LEAVE_CHAT, message)
     })
     socket.on(SEND_MESSAGE, (message) => {
         io.emit(SEND_MESSAGE, message)
     })
 })
+
+function generateRandomPosition(exclusiveMaximunValue) {
+    return Math.floor(Math.random() * exclusiveMaximunValue)
+}
 
 io.listen(process.env.PORT || 8080)
 console.info(`Chat Server running on port ${process.env.PORT || 8080}`)
